0028692266b76d003eea1b6e4447a0424a932534
abdulfaizp <abdulfaizp@gmail.com>
2017-02-20 14:30:18 +0000

    parsing done

---
+++ src/solution/main.py
@@ -0,0 +1,23 @@
+
+def main():
+    data = open("robots.mat.txt")
+
+    a = data.readlines()
+
+    for line in a[:3]:
+        _, cords = line.split(':')
+        obstacles = []
+        robot_pos = cords
+        if '#' in cords:
+            robot_pos, obstacles = cords.split('#')
+
+        if obstacles:
+            obstacles = obstacles.split(';')
+            obstacles[-1].replace('\n', '')
+            obstacles = list(map(lambda k: eval(k), obstacles))
+
+        robot_pos = eval("[" + robot_pos.replace("\n", "") + "]")
+        print(robot_pos)
+
+if __name__ == "__main__":
+    main()
