f1b120ed5c2a29a80f81574f34197bc80441089e
Mujavid Bukhari <sirmuj@gmail.com>
2017-02-20 15:43:19 +0000

    Update json structure

---
+++ src/simulation/index.js
@@ -0,0 +1,27 @@
+const data = {
+    //ARRAY of point locations for robot starting points
+    "robots": [
+        [160, 240],
+        [65, 30]
+    ],
+
+    //ARRAY of obstacles
+    "obstacles": [
+
+        //ARRAY of points making up obstacle (polygon)
+        [
+            [5,	5],
+            [20, 5],
+            [20, 10],
+            [5,	10]
+        ]
+    ]
+}
+
+const setup = () => {
+
+}
+
+const drawPolygon = obstacle => {
+
+}
